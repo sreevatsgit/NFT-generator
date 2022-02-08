8f22a0b78c3e0f8d64ae0ba2c7d3598bb1543bb9
Sreevatsan <sreevats.git@gmail.com>
2022-02-08 08:39:18 +0800

    TRIAL 1

--- index.js
+++ index.js
@@ -6,16 +6,50 @@ const canvas = createCanvas(width,height);
 const ctx = canvas.getContext("2d");
 //for number of images that you wanna generate mess around with edition
 const edition = myArgs.length > 0 ? Number(myArgs[0]): 1;
+var metadata = [];
+var attributes = [];
+var hash = [];
+var decodedHash = [];
 
 
 const saveLayer = (_canvas, _edition) => {
     fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer("image/png"));
-    console.log("Image created");
+    
 
-}
+};
+const addMetadata = (_edition) => {
+    let dateTime = Date.now();
+    let tempMetadata = {
+        hash: hash.join(""),
+        decodedHash: decodedHash,
+        edition: _edition,
+        date: dateTime,
+        attributes: attributes,  
+    };
+    metadata.push(tempMetadata);
+    attributes = [];
+    hash = [];
+    decodedHash = [];
+};
+
+
+const addAttributes = (_element, _layer) => {
+    let tempAttr = {
+        id: _element.id,
+        layer: _layer.name,
+        name: _element.name,
+        rarity: _element.rarity
+    }
+    attributes.push(tempAttr);
+    hash.push(_layer.id);
+    hash.push(_element.id);
+    decodedHash.push({[_layer.id]: _element.id})
+};
+
 
 const drawLayer = async (_layer, _edition) => {
     let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)]; 
+    //addMetadata(element, _layer);
     const image = await loadImage(`${_layer.location}${element.fileName}`);
     ctx.drawImage(
         image,
@@ -30,7 +64,13 @@ const drawLayer = async (_layer, _edition) => {
 for (let i=1; i<=edition; i++) {
     layers.forEach((layer) => {
         drawLayer(layer, i);
-    })
+    });
+    addMetadata(i);
     console.log("creating edition" + i);
-
 } 
+
+fs.readFile("./output/_metadata.json", (err,data)=>{
+    if(err) throw err;
+    fs.writeFileSync("./output/_metadata.json", JSON.stringify(metadata));
+
+});
